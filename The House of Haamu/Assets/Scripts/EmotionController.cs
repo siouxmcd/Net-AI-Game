2667887722bbf063ab6fe16c71f2e888022694b2
siouxmcd <claeomcdermott@u.boisestate.edu>
2018-09-14 10:42:18 -0600

    changed gitignore, some other settings, file structure

---
+++ The House of Haamu/Assets/Scripts/EmotionController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EmotionController : NetworkBehaviour
+{
+    public int CurrentGhostEmotionalValue;
+    public int LowestEmotionalValue = -400;
+
+    private int PreviousEmotionalValue;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // this checks to see if the players cause the ghosts emotional value
+        if (CurrentGhostEmotionalValue < LowestEmotionalValue)
+        {
+            CurrentGhostEmotionalValue = LowestEmotionalValue;
+        }
+
+		if (CurrentGhostEmotionalValue != PreviousEmotionalValue)
+        {
+            if (CurrentGhostEmotionalValue >= 200)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+            else if (CurrentGhostEmotionalValue < 200 && CurrentGhostEmotionalValue >= 100)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+            else if (CurrentGhostEmotionalValue < 100 && CurrentGhostEmotionalValue >= 50)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+            else if (CurrentGhostEmotionalValue < 50 && CurrentGhostEmotionalValue >= -50)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+            else if (CurrentGhostEmotionalValue < -50 && CurrentGhostEmotionalValue >= -100)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+            else if (CurrentGhostEmotionalValue < -100 && CurrentGhostEmotionalValue >= -200)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+            else if (CurrentGhostEmotionalValue < -200 && CurrentGhostEmotionalValue >= LowestEmotionalValue)
+            {
+                PreviousEmotionalValue = CurrentGhostEmotionalValue;
+            }
+        }
+	}
+}
